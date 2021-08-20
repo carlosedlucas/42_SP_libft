f13a293473ea71dc677e84a8d71a069d2e3b259d
carlosedlucas <carlosedlucas@gmail.com>
2021-08-20 17:26:14 -0300

    agoraaaaaaaaaaaa

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: carlosed <carlosed@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 14:13:02 by carlosed          #+#    #+#             */
-/*   Updated: 2021/08/20 17:15:03 by carlosed         ###   ########.fr       */
+/*   Updated: 2021/08/20 17:25:48 by carlosed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ char	ft_striteri(char *s, void (*f) (unsigned int, char))
 	unsigned int	i;
 
 	i = 0;
-	if (s && f)
+	if (!s || !f)
 	{
-		while (s[i])
-		{
-			f(i, s[i]);
-			i++;
-		}
+		return ;
 	}
-	return (0);
+	while (s[i])
+	{
+		f(i, s + i);
+		i++;
+	}
 }
