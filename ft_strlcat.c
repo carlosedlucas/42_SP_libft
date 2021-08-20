2d2fb07cc283939f8329ec48eb41b2d5c7157426
carlosedlucas <carlosedlucas@gmail.com>
2021-08-20 18:52:53 -0300

    ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: carlosed <carlosed@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 18:19:47 by carlosed          #+#    #+#             */
-/*   Updated: 2021/08/10 18:20:51 by carlosed         ###   ########.fr       */
+/*   Updated: 2021/08/20 18:52:22 by carlosed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,20 @@ size_t	ft_strlcat(char *dest, char *src, size_t size)
 	size_t	len;
 
 	i = 0;
-	len = 0;
-	while (dest[len] && len < size)
-		len++;
-	i = len;
-	while (src[len - i] && (len + 1) < size)
+	len = ft_strlen(dest);
+	if (size <= len)
 	{
-		dest[len] = src[len - i];
-		len++;
+		return (size + ft_strlen(src));
 	}
-	if (i < size)
-		dest[len] = '\0';
-	return (i + ft_strlen(src));
+	if (size > len)
+	{
+		while ((size - len) > 1 && src[i] != '\0')
+		{
+			dest[len + i] = src[i];
+			i++;
+			size--;
+		}
+		dest[len + i] = '\0';
+	}
+	return (len + ft_strlen(src));
 }
