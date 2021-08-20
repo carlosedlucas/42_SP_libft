e9b130df25106e21bbf884029ff87164b994087a
carlosedlucas <carlosedlucas@gmail.com>
2021-08-20 17:47:58 -0300

    umahoraacaba!

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: carlosed <carlosed@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 18:19:51 by carlosed          #+#    #+#             */
-/*   Updated: 2021/08/20 17:39:19 by carlosed         ###   ########.fr       */
+/*   Updated: 2021/08/20 17:47:33 by carlosed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
-	if (!src)
-		return (NULL);
 	i = 0;
+	if (size == 0)
+	{
+		dst[ft_strlen(dst)] = '\0';
+		return (ft_strlen(src));
+	}
 	if (size > 0)
 	{
 		while (src[i] != '\0' && (i < (size - 1)))
@@ -28,8 +31,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 		}
 		dst[i] = '\0';
 	}
-	if (size == 0)
-		dst[ft_strlen(dst)] = '\0';
 	while (src[i])
 		i++;
 	return (i);
